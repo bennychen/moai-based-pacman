56f421319f1c3502a64f8d408a7530ad74154525
Benny <benny@localhost.(none)>
2011-08-16 09:53:47 +0800

    add several checking functions for game map

--- GameMap.lua
+++ GameMap.lua
@@ -7,7 +7,7 @@
 -- REQUIREMENTS:  ---
 --         BUGS:  ---
 --        NOTES:  ---
---       AUTHOR:   (), <>
+--       AUTHOR:   (Benny Chen), <rockerbenny@gmail.com>
 --      COMPANY:  
 --      VERSION:  1.0
 --      CREATED:  08/10/2011 14:31:02 CST
@@ -29,20 +29,9 @@ TILE_MAP_ITEM_WALL_3 = 6
 
 GameMap = class()
 
-function GameMap:init( fileName )
-	local tileImageWidth = 64
-	local tileImageHeight = 64
-	local tileNumColumn = 3
-	local tileNumRow = 2
-	self.tileDeck = MOAITileDeck2D.new()
-	self.tileDeck:setTexture( "media/" .. map.tilesets[1].image )
-	self.tileDeck:setSize( tileNumColumn, tileNumRow, 
-			map.tilewidth / tileImageWidth, map.tileheight / tileImageHeight )
-
+function GameMap:init()
 	self.tileGrid = MOAIGrid.new()
-	local drawTileWidth = SCREEN_WIDTH / map.width
-	local drawTileHeight = drawTileWidth 
-	self.tileGrid:setSize( map.width, map.height, drawTileWidth, drawTileHeight )
+	self.tileGrid:setSize( map.width, map.height, gridSize, gridSize )
 	self.mapData = map.layers[1].data
 	for i = 1, map.height
 	do
@@ -55,10 +44,16 @@ function GameMap:init( fileName )
 	self.remapper = MOAIDeckRemapper.new()
 	self.remapper:reserve ( map.width * map.height )
 
+	self.width = gridSize * map.width
+	self.height = gridSize * map.height
+
 	self.tileMap = MOAIProp2D.new()
-	self.tileMap:setDeck( self.tileDeck )
+	self.tileMap:setDeck( TILE_DECK_2D_MAP )
 	self.tileMap:setGrid( self.tileGrid )
-	self.tileMap:setLoc( -drawTileWidth * map.width / 2, -drawTileHeight * map.height / 2 )
+	self.tileMap:setLoc( -self.width / 2, -self.height / 2 )
+
+	self.leftTopCorner = MOAITransform.new()
+	self.leftTopCorner:setLoc( -self.width / 2, self.height / 2 )
 end
 
 function GameMap:drawMap( layer )
@@ -69,11 +64,53 @@ function GameMap:clearMap( layer )
 	layer:removeProp( self.tileMap )
 end
 
-function GameMap:IsGridEmpty( gridX, gridY )
+function GameMap:getGridIndex( posX, posY )
+end
+
+function GameMap:isGridBean( gridX, gridY )
 	if ( gridX <= 0 or gridY <= 0 or 
 		 gridX > map.width or gridY > map.height )
 	then
-		print( "ERROR @ GameMap::IsGridEmpty - grid x or y is overflowed!!!" )
+		print( "ERROR @ GameMap::isGridBean - grid x[" .. gridX .. 
+				"] or y[" .. gridY .. "] is overflowed!!!" )
+		return
+	end
+	return gridData == self.mapData[( gridY - 1 ) * map.width + gridX] == TILE_MAP_ITEM_BEAN
+end
+
+function GameMap:isGridSuperBean( gridX, gridY )
+	if ( gridX <= 0 or gridY <= 0 or 
+		 gridX > map.width or gridY > map.height )
+	then
+		print( "ERROR @ GameMap::isGridSuperBean - grid x[" .. gridX .. 
+				"] or y[" .. gridY .. "] is overflowed!!!" )
+		return
+	end
+	local gridData = self.mapData[( gridY - 1 ) * map.width + gridX]
+	return gridData == TILE_MAP_ITEM_SUPER_BEAN_1 or
+		   gridData == TILE_MAP_ITEM_SUPER_BEAN_2
+end
+
+function GameMap:isGridWall( gridX, gridY )
+	if ( gridX <= 0 or gridY <= 0 or 
+		 gridX > map.width or gridY > map.height )
+	then
+		print( "ERROR @ GameMap::isGridWall - grid x[" .. gridX .. 
+				"] or y[" .. gridY .. "] is overflowed!!!" )
+		return
+	end
+	local gridData = self.mapData[( gridY - 1 ) * map.width + gridX]
+	return gridData == TILE_MAP_ITEM_WALL_1 or
+		   gridData == TILE_MAP_ITEM_WALL_2 or 
+		   gridData == TILE_MAP_ITEM_WALL_3
+end
+
+function GameMap:isGridEmpty( gridX, gridY )
+	if ( gridX <= 0 or gridY <= 0 or 
+		 gridX > map.width or gridY > map.height )
+	then
+		print( "ERROR @ GameMap::isGridEmpty - grid x[" .. gridX .. 
+				"] or y[" .. gridY .. "] is overflowed!!!" )
 		return
 	end
 	return self.mapData[( gridY - 1 ) * map.width + gridX] == TILE_MAP_ITEM_EMPTY
